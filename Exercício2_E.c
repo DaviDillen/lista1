5fa446e7d31a04e8106dac5bc924e496599cf542
DaviDillen <dillenburgdavi@gmail.com>
2024-09-13 15:45:34 -0300

    Update Exercício2_E.c

--- Exercício2_E.c
+++ Exercício2_E.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <math.h>
 int main () {
-    float latas ;
+    //declaração de variáveis
     float altura, largura;
-
-    printf("digit a altura: ");
+    //entrada de dados
+    printf("digite a altura em ml: ");
     scanf ( "%f" , &altura );
-    printf("digit a largura: ");
+    printf("digite a largura em ml: ");
     scanf ( "%f" , &largura );
-    printf("%f",largura * altura * 0.15 );
-
+    //processamento
+    float quantidade_de_tinta = largura * altura * 300;
+    float latas = ceil(quantidade_de_tinta / 2000);
+    //saída de dados
+    printf("%.2f", latas);
 
     return 0;
 }
