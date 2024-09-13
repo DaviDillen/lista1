75fcc440980675bdb16433a0e9363ab7091e661b
DaviDillen <dillenburgdavi@gmail.com>
2024-09-13 15:59:06 -0300

    Update F.c

--- F.c
+++ F.c
@@ -7,3 +7,16 @@ int main () {
 
     return 0;
 }
+
+#include <stdio.h>
+int main () {
+    float graus_farenheit;
+    printf("temperatura em graus farenheit ");
+    scanf ( "%f" , &graus_farenheit );
+    float graus_celsius = (graus_farenheit - 32) * 5 / 9;
+
+    //saída de dados
+    printf("%f", graus_celsius);
+
+    return 0;
+}
